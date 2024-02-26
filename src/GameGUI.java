9d485dad2b10c1805f4781adbef25df20fddd816
Matthew <matthewfox2000@gmail.com>
2024-02-26 10:10:27 -0400

    Made a better form with GroupLayout

--- src/GameGUI.java
+++ src/GameGUI.java
@@ -1,2 +1,42 @@
-public class GameGUI {
+import javax.swing.*;
+import javax.swing.GroupLayout;
+/*
+ * Created by JFormDesigner on Mon Feb 26 10:06:46 AST 2024
+ */
+
+
+
+/**
+ * @author Matthew
+ */
+public class GameGUI extends JFrame {
+    public GameGUI() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - Matthew
+
+        //======== this ========
+        var contentPane = getContentPane();
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 398, Short.MAX_VALUE)
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 269, Short.MAX_VALUE)
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - Matthew
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
