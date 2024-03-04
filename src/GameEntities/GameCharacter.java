e60b3ad24ae4de57c3f2700cc61a1840c0dbde9a
Matthew <matthewfox2000@gmail.com>
2024-03-04 08:48:50 -0400

    Added game entity object

---
+++ src/GameEntities/GameCharacter.java
@@ -0,0 +1,15 @@
+package GameEntities;
+
+public class GameCharacter {
+
+    int health_points;
+    int armor_points;
+    int strength_points;
+    int dexterity_points;
+
+    public void attack(){}
+
+    public void defend(){}
+
+    public void die(){}
+}
