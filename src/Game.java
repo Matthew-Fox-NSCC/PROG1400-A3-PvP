af018559b7691c7534b5aa9f648c5dfcfc597b8d
Matthew <matthewfox2000@gmail.com>
2024-03-18 10:08:14 -0300

    Removed some comments

--- src/Game.java
+++ src/Game.java
@@ -5,9 +5,8 @@ import javax.swing.*;
 public class Game {
 
     public void Play() {
-// schedule this for the event dispatch thread (edt)
-        SwingUtilities.invokeLater(PanelProcessing::displayJFrame); // end inner class The semicolon after means that it is a class.
-    } // end main
+        SwingUtilities.invokeLater(PanelProcessing::displayJFrame);
+    }
 
     public void battle() {
         double secondsToSleep = 2.0;
--- src/guiCode/PanelProcessing.java
+++ src/guiCode/PanelProcessing.java
@@ -10,13 +10,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PanelProcessing {
-    static JFrame frame = new JFrame("PVP Game"); // You can leave it here or not.
-    static SplashScreen splashPage = new SplashScreen(); // Represents each the container
+    static JFrame frame = new JFrame("PVP Game");
+    static SplashScreen splashPage = new SplashScreen();
     static PlayerArmorySelection armoryPage = new PlayerArmorySelection();
     static BattleScreen battlePage = new BattleScreen();
 
     //region displayFrame
-    public static void displayJFrame() { // method that it is gonna be called
+    public static void displayJFrame() {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(600, 600));
         frame.pack();
@@ -28,27 +28,24 @@ public class PanelProcessing {
 
     //region displayPanels
 
-    private static void displayPanelSplash()//one for each page
-    {
+    private static void displayPanelSplash() {
         frame.add(splashPage.panel);
         frame.repaint();
-        frame.validate(); // is necessary
+        frame.validate();
     }
 
-    private static void displayPanelArmory()//one for each page
-    {
-        frame.remove(splashPage.panel); // Takes away the previous panel.
+    private static void displayPanelArmory() {
+        frame.remove(splashPage.panel);
         frame.add(armoryPage.panel);
         frame.repaint();
-        frame.validate(); // is necessary
+        frame.validate();
     }
 
-    private static void displayPanelBattle()//one for each page
-    {
-        frame.remove(armoryPage.panel); // Takes away the previous panel.
+    private static void displayPanelBattle() {
+        frame.remove(armoryPage.panel);
         frame.add(battlePage.panel);
         frame.repaint();
-        frame.validate(); // is necessary
+        frame.validate();
 
         // TODO move enemy icon to enemy class
 //        ImageIcon enemyIcon = new ImageIcon(new javax.swing.ImageIcon(Objects.requireNonNull(getClass().getResource("pictures/enemies/aboleth.jpg"))).getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH));
