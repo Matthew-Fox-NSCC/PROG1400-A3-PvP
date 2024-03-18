f3711e6cfbfb3964eaeabae3e1721b0276d3cf4f
Matthew <matthewfox2000@gmail.com>
2024-03-18 09:30:14 -0300

    Added equipment to the player class

--- src/GameEntities/PlayerCharacter.java
+++ src/GameEntities/PlayerCharacter.java
@@ -2,8 +2,18 @@ package GameEntities;
 
 public class PlayerCharacter extends GameCharacter {
     public String classType;
+    private Equipment weapon;
+    private Equipment armor;
 
     public PlayerCharacter() {
         super();
+    }
+
+    public void equipWeapon(Equipment weapon) {
+        this.weapon = weapon;
+    }
+
+    public void equipArmor(Equipment armor) {
+        this.armor = armor;
     }
 }
