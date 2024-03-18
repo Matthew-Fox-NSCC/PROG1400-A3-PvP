617b8b4c6e9fc9900f4a03a00407e85933c24428
Matthew <matthewfox2000@gmail.com>
2024-03-18 11:46:50 -0300

    Decided to keep it for later

--- src/Game.java
+++ src/Game.java
@@ -6,20 +6,19 @@ public class Game {
 
     public void Play() {
         SwingUtilities.invokeLater(PanelProcessing::displayJFrame);
-
-        double secondsToSleep = 2.0;
-        boolean done = false;
-        while (!done) {
-            done = update();
-            try {
-                Thread.sleep((int) (secondsToSleep * 1000));
-            } catch (InterruptedException e) {
-                System.out.println(e.getMessage());
-            }
-        }
     }
 
     public void battle() {
+//        double secondsToSleep = 2.0;
+//        boolean done = false;
+//        while (!done) {
+//            done = update();
+//            try {
+//                Thread.sleep((int) (secondsToSleep * 1000));
+//            } catch (InterruptedException e) {
+//                System.out.println(e.getMessage());
+//            }
+//        }
     }
 
     public boolean update() {
