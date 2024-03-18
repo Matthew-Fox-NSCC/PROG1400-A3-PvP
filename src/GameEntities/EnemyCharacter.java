b49cbb836678c5786d5a7d315845f9367feb6c14
Matthew <matthewfox2000@gmail.com>
2024-03-18 09:34:00 -0300

    Filled the character constructors

--- src/GameEntities/EnemyCharacter.java
+++ src/GameEntities/EnemyCharacter.java
@@ -1,7 +1,11 @@
 package GameEntities;
 
 public class EnemyCharacter extends GameCharacter {
-    public EnemyCharacter() {
-        super();
+    public EnemyCharacter(int health_points, int armor_points, int strength_points, int dexterity_points) {
+        super(health_points, armor_points, strength_points, dexterity_points);
+    }
+
+    public EnemyCharacter(int health_points, int armor_points, int strength_points, int dexterity_points, String imageURL) {
+        super(health_points, armor_points, strength_points, dexterity_points, imageURL);
     }
 }
--- src/GameEntities/PlayerCharacter.java
+++ src/GameEntities/PlayerCharacter.java
@@ -5,8 +5,12 @@ public class PlayerCharacter extends GameCharacter {
     private Equipment weapon;
     private Equipment armor;
 
-    public PlayerCharacter() {
-        super();
+    public PlayerCharacter(int health_points, int armor_points, int strength_points, int dexterity_points) {
+        super(health_points, armor_points, strength_points, dexterity_points);
+    }
+
+    public PlayerCharacter(int health_points, int armor_points, int strength_points, int dexterity_points, String imageURL) {
+        super(health_points, armor_points, strength_points, dexterity_points, imageURL);
     }
 
     public void equipWeapon(Equipment weapon) {
