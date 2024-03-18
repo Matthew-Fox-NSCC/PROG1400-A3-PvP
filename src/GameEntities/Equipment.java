6895512192cf391eac05a55fe52c718c6b748369
Matthew <matthewfox2000@gmail.com>
2024-03-18 09:27:45 -0300

    Added the image url to the characters and equipment

--- src/GameEntities/Equipment.java
+++ src/GameEntities/Equipment.java
@@ -6,11 +6,19 @@ public class Equipment {
     private int minimum;
     private int maximum;
     private String name;
+    private String imageURL;
 
     public Equipment(String name, int minimum, int maximum) {
         this.name = name;
         this.minimum = minimum;
         this.maximum = maximum;
+    }
+
+    public Equipment(String name, int minimum, int maximum, String imageURL) {
+        this.name = name;
+        this.minimum = minimum;
+        this.maximum = maximum;
+        this.imageURL = imageURL;
     }
 
     public int calc() {
--- src/GameEntities/GameCharacter.java
+++ src/GameEntities/GameCharacter.java
@@ -2,12 +2,25 @@ package GameEntities;
 
 public abstract class GameCharacter {
 
-    int health_points;
-    int armor_points;
-    int strength_points;
-    int dexterity_points;
+    private int health_points;
+    private int armor_points;
+    private int strength_points;
+    private int dexterity_points;
+    private String imageURL;
 
-    public GameCharacter() {
+    public GameCharacter(int health_points, int armor_points, int strength_points, int dexterity_points) {
+        this.health_points = health_points;
+        this.armor_points = armor_points;
+        this.strength_points = strength_points;
+        this.dexterity_points = dexterity_points;
+    }
+
+    public GameCharacter(int health_points, int armor_points, int strength_points, int dexterity_points, String imageURL) {
+        this.health_points = health_points;
+        this.armor_points = armor_points;
+        this.strength_points = strength_points;
+        this.dexterity_points = dexterity_points;
+        this.imageURL = imageURL;
     }
 
     public void attack() {
