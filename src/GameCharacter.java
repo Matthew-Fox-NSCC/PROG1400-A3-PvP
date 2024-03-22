4594cb02603c524e9554063b88b9e38b487fe75b
Matthew Fox <25204673+Parascythe42@users.noreply.github.com>
2024-03-22 09:40:18 -0300

    Sorted everything into regions

--- src/GameCharacter.java
+++ src/GameCharacter.java
@@ -3,13 +3,15 @@ import java.awt.*;
 import java.util.Objects;
 
 public abstract class GameCharacter {
-
+    //region Variables
     protected int health_points;
     protected int armor_points;
     protected int strength_points;
     protected int dexterity_points;
     protected String imageURL;
+    //endregion
 
+    //region Constructors
     public GameCharacter(int health_points, int armor_points, int strength_points, int dexterity_points) {
         this.health_points = health_points;
         this.armor_points = armor_points;
@@ -26,45 +28,27 @@ public abstract class GameCharacter {
     }
 
     public GameCharacter() {}
+    //endregion
 
+    //region Getters
     public int getHealth_points() {
         return health_points;
-    }
-
-    public void setHealth_points(int health_points) {
-        this.health_points = health_points;
     }
 
     public int getArmor_points() {
         return armor_points;
     }
 
-    public void setArmor_points(int armor_points) {
-        this.armor_points = armor_points;
-    }
-
     public int getStrength_points() {
         return strength_points;
-    }
-
-    public void setStrength_points(int strength_points) {
-        this.strength_points = strength_points;
     }
 
     public int getDexterity_points() {
         return dexterity_points;
     }
 
-    public void setDexterity_points(int dexterity_points) {
-        this.dexterity_points = dexterity_points;
-    }
-
     public String getImageURL() {
         return imageURL;
-    }
-
-    public void setImageURL(String imageURL) {
-        this.imageURL = imageURL;
     }
 
     public ImageIcon getImageIcon() {
@@ -74,14 +58,39 @@ public abstract class GameCharacter {
     public ImageIcon getImageIcon(int width, int height) {
         return new ImageIcon(new javax.swing.ImageIcon(Objects.requireNonNull(getClass().getResource(imageURL))).getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH));
     }
+    //endregion
 
-    public int attack(GameCharacter character) {
-        return 0;
+    //region Setters
+    public void setHealth_points(int health_points) {
+        this.health_points = health_points;
     }
 
-    public void die() {
+    public void setArmor_points(int armor_points) {
+        this.armor_points = armor_points;
     }
 
+    public void setStrength_points(int strength_points) {
+        this.strength_points = strength_points;
+    }
+
+    public void setDexterity_points(int dexterity_points) {
+        this.dexterity_points = dexterity_points;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+    //endregion
+
+    //region CharacterMethods
+    public abstract int attack();
+
+    public abstract int defend();
+
+    public abstract void die();
+    //endregion
+
+    //region toString
     @Override
     public String toString() {
         return "GameCharacter{" +
@@ -92,4 +101,5 @@ public abstract class GameCharacter {
                 ", imageURL='" + imageURL + '\'' +
                 '}';
     }
+    //endregion
 }
