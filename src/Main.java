f15a48534a2d570e8ce61ec942a84a7b047f69aa
Matthew Fox <25204673+Parascythe42@users.noreply.github.com>
2024-03-23 16:46:15 -0300

    Changed to lambda and removed unused variable

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    Application app = new Application();
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
+        EventQueue.invokeLater(() -> {
+            try {
+                new Application();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         });
     }
